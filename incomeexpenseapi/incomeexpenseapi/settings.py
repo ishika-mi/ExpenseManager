1a5129c963ab54c8ef8ccc03a5334172ceb935f4
Ishika Shah <ishika.shah@mindinventory.com>
2024-02-05 13:13:00 +0530

    add pagination

--- incomeexpenseapi/incomeexpenseapi/settings.py
+++ incomeexpenseapi/incomeexpenseapi/settings.py
@@ -104,7 +104,9 @@ REST_FRAMEWORK = {
     'NON_FIELD_ERRORS_KEY': 'error',
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    )
+    ),
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
 
 # Password validation
