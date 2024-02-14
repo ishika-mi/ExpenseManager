56cf1799ce1a97c35a8c5f303e6142e64a0b5d64
Ishika Shah <ishika.shah@mindinventory.com>
2024-02-14 12:22:08 +0530

    added service layer in expense module and removed unnecessary comments

---
+++ incomeexpenseapi/expenses/services.py
@@ -0,0 +1,69 @@
+from expenses.models import Expense
+from django.http import JsonResponse
+
+
+class ExpenseListService:
+    def __init__(self, request, serializer_class, permissions_classes):
+        self.request = request
+        self.serializer_class = serializer_class
+        self.permissions_classes = permissions_classes
+        
+    def get_queryset(self):
+        user = self.request.user
+        return Expense.objects.raw(
+            "SELECT * FROM expenses_expense WHERE owner_id = %s", [user.id]
+        )
+    
+    def list_view(self):
+        expenses = self.get_queryset()
+        serializer = self.serializer_class(expenses, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    
+    def post_view(self):
+        serializer = self.serializer_class(data=self.request.data)
+        if serializer.is_valid():
+            serializer.save(owner=self.request.user)
+            return JsonResponse(serializer.data, status=201)
+        return JsonResponse(serializer.errors, status=400)
+
+class ExpenseDetailService:
+    def __init__(self, request, serializer_class, permissions_classes,id):
+        self.request = request
+        self.serializer_class = serializer_class
+        self.permissions_classes = permissions_classes
+        self.id = id
+    
+    def get_object(self):
+        user = self.request.user
+        try:
+            return Expense.objects.raw(
+                "SELECT * FROM expenses_expense WHERE id = %s AND owner_id = %s",
+                [self.id, user.id],
+            )[0]
+        except Expense.DoesNotExist:
+            return None
+    
+    def get_view(self):
+        expense = self.get_object()
+        if expense is not None:
+            serializer = self.serializer_class(expense)
+            return JsonResponse(serializer.data)
+        else:
+            return JsonResponse({"error": "Expense not found"}, status=404)
+    
+    def put_view(self):
+        expense = self.get_object()
+        if expense is None:
+            return JsonResponse({"error": "Expense not found"}, status=404)
+        serializer = self.serializer_class(expense, data=self.request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data)
+        return JsonResponse(serializer.errors, status=400)
+    
+    def delete_view(self):
+        expense = self.get_object()
+        if expense is None:
+            return JsonResponse({"error": "Expense not found"}, status=404)
+        expense.delete()
+        return JsonResponse({"message": "Expense deleted successfully"})
--- incomeexpenseapi/expenses/views.py
+++ incomeexpenseapi/expenses/views.py
@@ -1,89 +1,3 @@
-# from django.shortcuts import render
-# from django.db import connection
-# from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-# from expenses.models import Expense
-# from rest_framework import permissions,status
-# from expenses.permissions import IsOwner
-# from expenses.serializers import ExpensesSerializers
-# from rest_framework.response import Response
-
-# class ExpenseListAPIView(ListCreateAPIView):
-#     serializer_class = ExpensesSerializers
-#     permission_classes = (permissions.IsAuthenticated,)
-
-#     def get_queryset(self):
-#         user_id = self.request.user.id
-#         return Expense.objects.raw("SELECT * FROM expenses_expense WHERE owner_id = %s", [user_id])
-
-#     def perform_create(self, serializer):
-#         serializer.save(owner=self.request.user)
-
-#     def post(self, request, *args, **kwargs):
-#         serializer = self.get_serializer(data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         self.perform_create(serializer)
-#         return Response(serializer.data)
-
-# class ExpenseDetailAPIView(RetrieveUpdateDestroyAPIView):
-#     serializer_class = ExpensesSerializers
-#     permission_classes = (permissions.IsAuthenticated, IsOwner,)
-#     lookup_field = "id"
-
-#     def get_queryset(self):
-#         user_id = self.request.user.id
-#         print(type(user_id),type(self.kwargs['id']),"-----------------------------")
-#         print(user_id,self.kwargs['id'],"-----------------------------")
-#         expense_id = self.kwargs['id']
-#         return Expense.objects.raw(f"SELECT * FROM expenses_expense WHERE id = {expense_id} AND owner_id = {user_id}")
-
-#     def perform_create(self, serializer):
-#         serializer.save(owner=self.request.user)
-
-#     def delete(self, request, *args, **kwargs):
-#         instance = self.get_object()
-#         self.perform_destroy(instance)
-#         return Response(status=status.HTTP_204_NO_CONTENT)
-
-#     def put(self, request, *args, **kwargs):
-#         instance = self.get_object()
-#         serializer = self.get_serializer(instance, data=request.data)
-#         serializer.is_valid(raise_exception=True)
-#         self.perform_update(serializer)
-#         return Response(serializer.data)
-
-
-
-# from django.shortcuts import render
-# from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-# from expenses.models import Expense
-# from rest_framework import permissions
-# from expenses.permissions import IsOwner
-
-# from expenses.serializers import ExpensesSerializers
-
-# class ExpenseListAPIView(ListCreateAPIView):
-#     serializer_class = ExpensesSerializers
-#     queryset = Expense.objects.all()
-#     permissions_classes = (permissions.IsAuthenticated)
-#     def perform_create(self, serializer):
-#         return serializer.save(owner = self.request.user)
-    
-#     def get_queryset(self):
-#         return self.queryset.filter(owner = self.request.user)
-
-# class ExpenseDetailAPIView(RetrieveUpdateDestroyAPIView):
-#     serializer_class = ExpensesSerializers
-#     queryset = Expense.objects.all()
-#     permissions_classes = (permissions.IsAuthenticated, IsOwner,)
-#     lookup_field = "id"
-
-#     def perform_create(self, serializer):
-#         return serializer.save(owner = self.request.user)
-    
-#     def get_queryset(self):
-#         return self.queryset.filter(owner = self.request.user)
-
-
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from rest_framework.generics import ListAPIView
@@ -92,66 +6,30 @@ from rest_framework import permissions
 from expenses.permissions import IsOwner
 
 from expenses.serializers import ExpensesSerializers
+from expenses.services import ExpenseListService, ExpenseDetailService
 
 class ExpenseListAPIView(ListAPIView):
     serializer_class = ExpensesSerializers
     permissions_classes = (permissions.IsAuthenticated,)
 
-    def get_queryset(self):
-        user = self.request.user
-        return Expense.objects.raw(
-            "SELECT * FROM expenses_expense WHERE owner_id = %s", [user.id]
-        )
-
     def list(self, request, *args, **kwargs):
-        expenses = self.get_queryset()
-        serializer = self.serializer_class(expenses, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return ExpenseListService(request=request,serializer_class=self.serializer_class,permissions_classes=self.permissions_classes).list_view()
+    
+    def post(self, request, format=None):
+        return ExpenseListService(request = request, serializer_class = self.serializer_class, permissions_classes = self.permission_classes).post_view()
 
 
 class ExpenseDetailAPIView(APIView):
     serializer_class = ExpensesSerializers
     permissions_classes = (permissions.IsAuthenticated, IsOwner,)
 
-    def get_object(self, id):
-        user = self.request.user
-        try:
-            return Expense.objects.raw(
-                "SELECT * FROM expenses_expense WHERE id = %s AND owner_id = %s",
-                [id, user.id],
-            )[0]
-        except Expense.DoesNotExist:
-            return None
-
     def get(self, request, id, format=None):
-        expense = self.get_object(id)
-        if expense is not None:
-            serializer = self.serializer_class(expense)
-            return JsonResponse(serializer.data)
-        else:
-            return JsonResponse({"error": "Expense not found"}, status=404)
+        return ExpenseDetailService(request = request, serializer_class = self.serializer_class, permissions_classes = self.permission_classes,id = id).get_view()
 
     def put(self, request, id, format=None):
-        expense = self.get_object(id)
-        if expense is not None:
-            serializer = self.serializer_class(expense, data=request.data)
-            if serializer.is_valid():
-                serializer.save()
-                return JsonResponse(serializer.data)
-            return JsonResponse(serializer.errors, status=400)
-        else:
-            return JsonResponse({"error": "Expense not found"}, status=404)
+        return ExpenseDetailService(request = request, serializer_class = self.serializer_class, permissions_classes = self.permission_classes,id = id).put_view()
 
     def delete(self, request, id, format=None):
-        expense = self.get_object(id)
-        if expense is None:
-            return JsonResponse({"error": "Expense not found"}, status=404)
-        expense.delete()
-        return JsonResponse({"message": "Expense deleted successfully"})
+        return ExpenseDetailService(request = request, serializer_class = self.serializer_class, permissions_classes = self.permission_classes,id = id).delete_view()
 
-    def post(self, request, format=None):
-        serializer = self.serializer_class(data=request.data)
-        if serializer.is_valid():
-            serializer.save(owner=self.request.user)
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+    
